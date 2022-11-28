c984d63f67e047027e1b877ef0496add485ee1c0
cc <1826515410@qq.com>
2022-11-28 12:42:31 +0800

    ✨新增(Vue): 创建Tag组件

---
+++ src/components/Tag/Tag.vue
@@ -0,0 +1,28 @@
+<template>
+  <view class="tag">{{ props.value }}</view>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  value: string
+  color?: string
+  bgColor?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  value: '',
+  color: '#fff',
+  bgColor: '#7a7acd'
+})
+</script>
+
+<style scoped lang="scss">
+.tag {
+  border-radius: $uni-border-radius-base;
+  background-color: v-bind(bgColor);
+  color: v-bind(color);
+  padding: 3px 5px;
+  font-size: 12px;
+  box-sizing: border-box;
+}
+</style>
