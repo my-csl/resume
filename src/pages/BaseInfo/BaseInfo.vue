0be8c6b660016e7bd15c8cffa6f52a04cda74d48
cc <1826515410@qq.com>
2022-11-28 12:45:10 +0800

    ✨新增(Vue): 首页增加教育经历板块

--- src/pages/BaseInfo/BaseInfo.vue
+++ src/pages/BaseInfo/BaseInfo.vue
@@ -1,11 +1,15 @@
 <template>
   <view class="base-info">
     <UserInfo></UserInfo>
+    <view class="box">
+      <EducationalExperience></EducationalExperience>
+    </view>
   </view>
 </template>
 
 <script setup lang="ts">
 import UserInfo from './component/UserInfo.vue'
+import EducationalExperience from './component/EducationalExperience.vue'
 </script>
 
 <style lang="scss">
@@ -15,5 +19,9 @@ page {
 
 .base-info {
   padding: $uni-padding-base;
+}
+
+.box {
+  margin-top: 16px;
 }
 </style>
--- src/pages/BaseInfo/component/EducationalExperience.vue
+++ src/pages/BaseInfo/component/EducationalExperience.vue
@@ -1,7 +1,37 @@
 <template>
-  <view class="educational-experience"> </view>
+  <view class="educational-experience">
+    <HeaderTitle title="教育经历" :border="true"></HeaderTitle>
+    <view class="info">
+      <text>2017.06-2020.06</text>
+      <text class="school">浙江工贸职业技术学院</text>
+      <text class="bold">大专</text>
+    </view>
+  </view>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.educational-experience {
+  border-radius: $uni-border-radius-lg;
+  background-color: $uni-bg-color-white;
+  padding: $uni-padding-base;
+  box-shadow: 0 0 15px $uni-bg-color;
+
+  .info {
+    display: flex;
+    font-size: 14px;
+    margin-top: 10px;
+    color: $uni-text-color-grey;
+
+    .school {
+      flex: 1;
+      text-align: center;
+    }
+
+    .bold {
+      font-weight: bold;
+    }
+  }
+}
+</style>
