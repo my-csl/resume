745eccaf72812f560fb89b70877e1016a5bf3635
cc <1826515410@qq.com>
2022-11-28 12:41:25 +0800

    ✨新增(Vue): 创建HederTitle组件

--- src/components/HeaderTitle/HeaderTitle.vue
+++ src/components/HeaderTitle/HeaderTitle.vue
@@ -1,6 +1,6 @@
 <template>
-  <view class="header-title">
-    <view class="circle" />
+  <view class="header-title" :class="{ border: props.border }">
+    <view class="circle"></view>
     <text class="title">
       {{ props.title }}
     </text>
@@ -19,4 +19,21 @@ const props = withDefaults(defineProps<Props>(), {
 })
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.header-title {
+  display: flex;
+  align-items: center;
+
+  &.border {
+    padding-bottom: 5px;
+    border-bottom: 1px solid #f5f5f5;
+  }
+}
+.circle {
+  width: 5px;
+  height: 5px;
+  background-color: $uni-color-primary;
+  border-radius: 50%;
+  margin-right: 6px;
+}
+</style>
