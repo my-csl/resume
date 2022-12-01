d32831b92f951759bd04111f48310a03abe69bb3
cc <1826515410@qq.com>
2022-12-01 21:24:24 +0800

    ✨新增(Vue): 首页增加基本信息模块和技能模块

--- src/pages/BaseInfo/BaseInfo.vue
+++ src/pages/BaseInfo/BaseInfo.vue
@@ -4,12 +4,20 @@
     <view class="box">
       <EducationalExperience></EducationalExperience>
     </view>
+    <view class="box">
+      <DetailInfo></DetailInfo>
+    </view>
+    <view class="box">
+      <Skill></Skill>
+    </view>
   </view>
 </template>
 
 <script setup lang="ts">
 import UserInfo from './component/UserInfo.vue'
 import EducationalExperience from './component/EducationalExperience.vue'
+import DetailInfo from './component/DetailInfo.vue'
+import Skill from './component/Skill.vue'
 </script>
 
 <style lang="scss">
