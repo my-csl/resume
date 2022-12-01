0922ec7c0102b45069d5d3e518fb9cb78143b770
cc <1826515410@qq.com>
2022-12-01 21:23:38 +0800

    ✨新增(Vue): 创建首页技能列表组件

---
+++ src/pages/BaseInfo/component/Skill.vue
@@ -0,0 +1,42 @@
+<template>
+  <view class="skill">
+    <HeaderTitle title="技能"></HeaderTitle>
+    <view class="tags">
+      <Tag v-for="tag in tags" :key="tag" class="tag" :value="tag"></Tag>
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+import Tag from '@/components/Tag/Tag.vue'
+
+const tags = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'VUE',
+  'Uni-App',
+  'ES6',
+  'Webpack',
+  'Vite',
+  'Scss',
+  'Git',
+  'npm'
+]
+</script>
+
+<style scoped lang="scss">
+.skill {
+  background-color: $uni-bg-color-white;
+  padding: $uni-padding-base;
+  border-radius: $uni-border-radius-lg;
+}
+.tags {
+  display: flex;
+  flex-wrap: wrap;
+
+  .tag {
+    margin: 10px 5px 0 0;
+  }
+}
+</style>
