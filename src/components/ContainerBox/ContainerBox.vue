df81303942d04e2d2313e1d46721c9590ecf24e4
cc <1826515410@qq.com>
2022-12-03 11:16:20 +0800

    ✨新增(Vue): 创建box容器组件

---
+++ src/components/ContainerBox/ContainerBox.vue
@@ -0,0 +1,16 @@
+<template>
+  <view class="container-box">
+    <slot></slot>
+  </view>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.container-box {
+  background-color: $uni-bg-color-white;
+  padding: $uni-padding-base;
+  border-radius: $uni-border-radius-lg;
+  box-shadow: 0 0 15px $uni-bg-color;
+}
+</style>
