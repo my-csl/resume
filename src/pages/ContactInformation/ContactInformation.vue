baa5d257bbf84bf344eace3d026de3658113c318
cc <1826515410@qq.com>
2022-12-03 17:30:18 +0800

    ✨新增(Vue): 完成联系方式页面内容

--- src/pages/ContactInformation/ContactInformation.vue
+++ src/pages/ContactInformation/ContactInformation.vue
@@ -1,7 +1,57 @@
 <template>
-  <view>ContactInformation</view>
+  <view class="contact-infomation">
+    <ContainerBox>
+      <HeaderTitle title="联系方式"></HeaderTitle>
+      <view v-for="item in contactList" :key="item.value" class="cell">
+        <image class="icon" :src="item.img" />
+        <text class="value">{{ item.value }}</text>
+        <view class="copy-btn" @click="copy(item.value)">复制</view>
+      </view>
+    </ContainerBox>
+  </view>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const contactList = [
+  { img: '/static/ContactInformation/phone.png', value: '15179773914' },
+  { img: '/static/ContactInformation/email.png', value: '1826515410@qq.com' },
+  { img: '/static/ContactInformation/wechat.png', value: '15179773914' },
+  { img: '/static/ContactInformation/qq.png', value: '1826515410' }
+]
 
-<style lang="scss" scoped></style>
+const copy = (value: string) => {
+  uni.setClipboardData({
+    data: value
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+.contact-infomation {
+  padding: $uni-padding-base;
+}
+.cell {
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  margin-top: 20px;
+
+  .icon {
+    width: 22px;
+    height: 22px;
+  }
+
+  .value {
+    flex: 1;
+    margin-left: 10px;
+  }
+
+  .copy-btn {
+    padding: 4px 12px;
+    background-color: $uni-color-primary;
+    border-radius: 30px;
+    color: #fff;
+    font-size: 12px;
+  }
+}
+</style>
