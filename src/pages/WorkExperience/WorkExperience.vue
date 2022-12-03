e8168c7a4633d9aa6f8ee483dea5dbd76b426867
cc <1826515410@qq.com>
2022-12-03 11:56:30 +0800

    ✨新增(Vue): 完善工作经历页面内容

--- src/pages/WorkExperience/WorkExperience.vue
+++ src/pages/WorkExperience/WorkExperience.vue
@@ -1,7 +1,51 @@
 <template>
-  <view> WorkExperience </view>
+  <view class="work-experience">
+    <ContainerBox>
+      <HeaderTitle title="工作经历"></HeaderTitle>
+      <view v-for="item in list" :key="item.name" class="item">
+        <ExperienceItem
+          :date="item.date"
+          :name="item.name"
+          :skill="item.skill"
+          :job="item.job"
+        ></ExperienceItem>
+      </view>
+    </ContainerBox>
+  </view>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ExperienceItem from './component/ExperienceItem.vue'
 
-<style scoped lang="scss"></style>
+const list = [
+  {
+    date: '2022.04-至今',
+    name: '柏斯速眠科技有限公司',
+    skill: '小程序、h5',
+    job: '小程序、h5组长'
+  },
+  {
+    date: '2020.06-2022.03',
+    name: '南昌耀启科技有限公司',
+    skill: 'h5、app、后台关系系统',
+    job: '前端开发'
+  }
+]
+</script>
+
+<style scoped lang="scss">
+.work-experience {
+  padding: $uni-padding-base;
+
+  .item {
+    margin-top: 15px;
+    border-bottom: 1px solid #f5f5f5;
+    padding-bottom: 10px;
+
+    &:last-child {
+      padding-bottom: 0;
+      border-bottom: none;
+    }
+  }
+}
+</style>
---
+++ src/pages/WorkExperience/component/ExperienceItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <view class="experience-item">
+    <view class="line">
+      <text class="date">{{ props.date }}</text>
+      <text class="name">{{ props.name }}</text>
+    </view>
+    <view class="line">
+      <text class="skill">{{ props.skill }}</text>
+      <text class="job">{{ props.job }}</text>
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  date: string
+  name: string
+  skill: string
+  job: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  date: '',
+  name: '',
+  skill: '',
+  job: ''
+})
+</script>
+
+<style scoped lang="scss">
+.line {
+  display: flex;
+  justify-content: space-between;
+  font-size: 12px;
+  line-height: 25px;
+}
+</style>
